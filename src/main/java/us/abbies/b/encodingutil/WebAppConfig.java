cbc9e647a49cd8eeb3e858c3c7b2c93b307a8c61
Steve McKay <shubalubdub@gmail.com>
2013-10-02 21:13:30 -0400

    add  bootstrap

--- src/main/java/us/abbies/b/encodingutil/WebAppConfig.java
+++ src/main/java/us/abbies/b/encodingutil/WebAppConfig.java
@@ -5,14 +5,16 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import us.abbies.b.encodingutil.view.JsonExceptionResolver;
 import us.abbies.b.encodingutil.view.JsonViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan
-public class WebAppConfig {
+public class WebAppConfig extends WebMvcConfigurerAdapter {
     @Bean
     public ViewResolver viewResolver() {
         return new JsonViewResolver();
@@ -22,4 +24,10 @@ public class WebAppConfig {
     public HandlerExceptionResolver handlerExceptionResolver() {
         return new JsonExceptionResolver();
     }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
 }
